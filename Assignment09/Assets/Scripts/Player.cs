0eaae64a9811c2c3d955a2e9fcabc4db36c70af1
jerrym92 <jerry_mayton@bloomfield.edu>
2021-03-11 21:00:03 -0600

    Update
    Re submit hw

---
+++ Assignment09/Assets/Scripts/Player.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    
+    public int level = 1;
+    public int experience = 0;
+    public int currentLevel;
+
+   
+    public Text playerLevelText;
+    public Text playerexperienceText;
+    public Text currentLevelText;
+
+    public void Save()
+    {
+        SaveLoad.Save(this);
+    }
+
+    public void Load()
+    {
+        PlayerData data = SaveLoad.Load();
+
+        
+        level = data.level;
+        experience = data.experience;
+        currentLevel = data.currentLevel;
+
+
+        if (currentLevel == 1)
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        if (currentLevel == 2)
+        {
+            SceneManager.LoadScene(2);
+        }
+
+        if (currentLevel == 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+  
+
+    void Awake()
+   {
+        GameObject[] gameController = GameObject.FindGameObjectsWithTag("GameController");
+        if(gameController.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+
+        GameObject[] canvas = GameObject.FindGameObjectsWithTag("UI");
+        if(canvas.Length > 1)
+        {
+            Destroy(canvas[1]);
+        }
+       DontDestroyOnLoad(canvas[0]);
+   }
+
+    void Update()
+    {
+        playerLevelText.text = "Player Level: " + level;
+
+        playerexperienceText.text = "EXP: " + experience;
+        currentLevelText.text = "Current Level: " + currentLevel;
+    }
+
+    public void PlayerLevelUp()
+    {
+        level += 1;
+    }
+
+    public void PlayerLevelDown()
+    {
+        level -= 1;
+    }
+
+   
+
+    public void PlayerexperienceUp()
+    {
+        experience += 1;
+    }
+
+    public void PlayerexperienceDown()
+    {
+        experience -= 1;
+    }
+
+    public void NextLevel()
+    {
+        currentLevel = SceneManager.GetActiveScene().buildIndex + 1;
+        LoadLevel();
+    }
+
+    public void PreviousLevel()
+    {
+        currentLevel = SceneManager.GetActiveScene().buildIndex - 1;
+        LoadLevel();
+    }
+
+    void LoadLevel()
+    {
+        SceneManager.LoadScene(currentLevel);
+    }
+}
---
+++ Assignment09/Assets/Scripts/SaveLoad.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SaveLoad
+{
+    public static void Save(Player player)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + 
+        Path.DirectorySeparatorChar  + "savedGames.gd");
+        PlayerData data = new PlayerData(player);
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    public static PlayerData Load()
+    {
+        if (File.Exists(Application.persistentDataPath + Path.DirectorySeparatorChar + 
+            "savedGames.gd"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + Path.DirectorySeparatorChar 
+                + "savedGames.gd", FileMode.Open);
+            PlayerData data = bf.Deserialize(file) as PlayerData;
+            file.Close();
+
+            return data;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    
+}
